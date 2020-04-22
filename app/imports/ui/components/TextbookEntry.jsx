7166aa47a7f456dde4acf55c58aa4e2e35097833
Sophia Elize Cruz <secruz@hawaii.edu>
2020-04-22 01:12:10 -1000

    worked on textbook entry component

--- app/imports/ui/components/TextbookEntry.jsx
+++ app/imports/ui/components/TextbookEntry.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Card, Image, Feed, Grid } from 'semantic-ui-react';
+import { Image, Grid } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
-class Contact extends React.Component {
+class TextbookEntry extends React.Component {
     render() {
         return (
             <Grid centered container>
                 <Grid.Column width={7}>
-                    <Image src={this.props.contact.image} floated='right' size='large' />
+                    <Image src={this.props.book.image} floated='right' size='large' />
                     {/*<Card.Header>{this.props.contact.firstName} {this.props.contact.lastName}</Card.Header>*/}
                     {/*<Card.Meta>*/}
                     {/*    <span>{this.props.contact.address}</span>*/}
@@ -18,27 +18,26 @@ class Contact extends React.Component {
                     {/*    {this.props.contact.description}*/}
                     {/*</Card.Description>*/}
                 </Grid.Column>
-                <Card.Content extra>
-                    <Link to={`/edit/${this.props.contact._id}`}>Edit</Link>
-                </Card.Content>
-                <Card.Content extra>
-                    <Feed>
-                        {this.props.notes.map((note, index) => <Note key={index} note={note}/>)}
-                    </Feed>
-                </Card.Content>
-                <Card.Content extra>
-                    <AddNote owner={this.props.contact.owner} contactId={this.props.contact._id}/>
-                </Card.Content>
+                <Grid.Column stackable={true} width={7}>
+                    Description: {this.props.book.description} <br />
+                    Condition: {this.props.book.condition} <br />
+                    Contact Seller: {this.props.userinfo.email} <br />
+                    Cost: <br />
+                    <Link to={`/edit/${this.props.book._id}`}>Edit</Link>
+                </Grid.Column>
+                {/*<Card.Content extra>*/}
+                {/*    <AddNote owner={this.props.contact.owner} contactId={this.props.contact._id}/>*/}
+                {/*</Card.Content>*/}
             </Grid>
         );
     }
 }
 
 /** Require a document to be passed to this component. */
-Contact.propTypes = {
-    contact: PropTypes.object.isRequired,
-    notes: PropTypes.array.isRequired,
+TextbookEntry.propTypes = {
+    book: PropTypes.object.isRequired,
+    userinfo: PropTypes.object.isRequired,
 };
 
 /** Wrap this component in withRouter since we use the <Link> React Router element. */
-export default withRouter(Contact);
+export default withRouter(TextbookEntry);
