236ef9fd09ab41bb9c964c4abb4f5db87d61aaa6
Sang Nguyen <60032493+sanngu68@users.noreply.github.com>
2020-05-11 21:40:58 -1000

    Finished merge

--- app/imports/ui/pages/EditProfile.jsx
+++ app/imports/ui/pages/EditProfile.jsx
@@ -10,67 +10,67 @@ import { UserInfo } from '../../api/userinfo/Userinfo';
 import SimpleSchema from "simpl-schema";
 
 const formSchema = new SimpleSchema({
-    name: String,
-    email: String,
-    id: Number,
-    image: String,
-    description: String,
+  name: String,
+  email: String,
+  id: Number,
+  image: String,
+  description: String,
 });
 
 /** Renders the Page for the user to edit their profile. */
 class EditProfile extends React.Component {
 
-    /** On successful submit, insert the data. */
-    submit(data) {
-        const {name, email, id, image, description, _id} = data;
-        UserInfo.update(_id, {$set: {name, email, id, image, description}}, (error) => (error ?
-            swal('Error', error.message, 'error') :
-            swal('Success', 'Profile updated successfully', 'success')));
-    }
+  /** On successful submit, insert the data. */
+  submit(data) {
+    const { name, email, id, image, description, _id } = data;
+    UserInfo.update(_id, { $set: { name, email, id, image, description } }, (error) => (error ?
+        swal('Error', error.message, 'error') :
+        swal('Success', 'Profile updated successfully', 'success')));
+  }
 
-    /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
-    render() {
-        return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
-    }
-    
-    /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
-    renderPage() {
-        return (
-            <Grid container inverted centered>
-                <Grid.Column>
-                    <Header as="h2" inverted textAlign="center">Edit Profile</Header>
-                    <AutoForm schema={formSchema} onSubmit={data => this.submit(data)} model={this.props.doc}>
-                        <Segment>
-                            <TextField name='name'/>
-                            <TextField name='email'/>
-                            <NumField name='id' decimal={false}/>
-                            <TextField name='image'/>
-                            <LongTextField name='description'/>
-                            <SubmitField value='Submit'/>
-                            <ErrorsField/>
-                        </Segment>
-                    </AutoForm>
-                </Grid.Column>
-            </Grid>
-        );
-    }
+  /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
+  render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
+
+  /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
+  renderPage() {
+    return (
+        <Grid container inverted centered>
+          <Grid.Column>
+            <Header as="h2" inverted textAlign="center">Edit Profile</Header>
+            <AutoForm schema={formSchema} onSubmit={data => this.submit(data)} model={this.props.doc}>
+              <Segment>
+                <TextField name='name'/>
+                <TextField name='email'/>
+                <NumField name='id' decimal={false}/>
+                <TextField name='image'/>
+                <LongTextField name='description'/>
+                <SubmitField value='Submit'/>
+                <ErrorsField/>
+              </Segment>
+            </AutoForm>
+          </Grid.Column>
+        </Grid>
+    );
+  }
 }
 
 /** Require the presence of a UserInfo document in the props object. Uniforms adds 'model' to the props, which we use.*/
 EditProfile.propTypes = {
-    doc: PropTypes.object,
-    model: PropTypes.object,
-    ready: PropTypes.bool.isRequired,
+  doc: PropTypes.object,
+  model: PropTypes.object,
+  ready: PropTypes.bool.isRequired,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
-export default withTracker(({match}) => {
-    // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
-    const documentId = match.params._id;
-    // Get access to UserInfo documents.
-    const subscription = Meteor.subscribe('UserInfo');
-    return {
-        doc: UserInfo.findOne(documentId),
-        ready: subscription.ready(),
-    };
+export default withTracker(({ match }) => {
+  // Get the documentID from the URL field. See imports/ui/layouts/App.jsx for the route containing :_id.
+  const documentId = match.params._id;
+  // Get access to UserInfo documents.
+  const subscription = Meteor.subscribe('UserInfo');
+  return {
+    doc: UserInfo.findOne(documentId),
+    ready: subscription.ready(),
+  };
 })(EditProfile);
--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -11,61 +11,61 @@ import TextbookEntry from '../components/TextbookEntry';
 /** Renders the Page containing the logged in user's information and books they are selling. */
 class Profile extends React.Component {
 
-    render() {
-        return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
-    }
+  render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
 
-    renderPage() {
-        return (
-            <Grid className="profile-back">
-                <Grid container>
-                    <Card centered fluid>
-                        <Card.Content>
-                            <Image
-                                floated='right'
-                                size='medium'
-                                src={this.props.userInfo.image}
-                            />
-                            <Card.Header>{this.props.userInfo.name} </Card.Header>
-                            <Card.Meta>Email: {this.props.userInfo.email}</Card.Meta>
-                            <Card.Meta>UH ID Number: {this.props.userInfo.id}</Card.Meta>
-                            <Card.Meta>
-                                {this.props.userInfo.description}
-                            </Card.Meta>
-                        </Card.Content>
-                        <Card.Content extra>
-                            <Link to={`/edit/${this.props.userInfo._id}`}>Edit</Link>
-                        </Card.Content>
-                    </Card>
-                    <Card.Group centered>
-                        {this.props.books.map((book, index) => <TextbookEntry
-                            key={index}
-                            book={book}
-                        />)}
-                    </Card.Group>
-                </Grid>
-            </Grid>
-        );
-    }
+  renderPage() {
+    return (
+        <Grid className="profile-back">
+          <Grid container>
+            <Card centered fluid>
+              <Card.Content>
+                <Image
+                    floated='right'
+                    size='medium'
+                    src={this.props.userInfo.image}
+                />
+                <Card.Header>{this.props.userInfo.name} </Card.Header>
+                <Card.Meta>Email: {this.props.userInfo.email}</Card.Meta>
+                <Card.Meta>UH ID Number: {this.props.userInfo.id}</Card.Meta>
+                <Card.Meta>
+                  {this.props.userInfo.description}
+                </Card.Meta>
+              </Card.Content>
+              <Card.Content extra>
+                <Link to={`/edit/${this.props.userInfo._id}`}>Edit</Link>
+              </Card.Content>
+            </Card>
+            <Card.Group centered>
+              {this.props.books.map((book, index) => <TextbookEntry
+                  key={index}
+                  book={book}
+              />)}
+            </Card.Group>
+          </Grid>
+        </Grid>
+    );
+  }
 }
 
 /** Require a document to be passed to this component. */
 Profile.propTypes = {
-    userInfo: PropTypes.object,
-    books: PropTypes.array.isRequired,
-    ready: PropTypes.bool.isRequired,
+  userInfo: PropTypes.object,
+  books: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
 };
 
 /** Wrap this component in withRouter since we use the <Link> React Router element. */
-export default withTracker(({match}) => {
-    // Get access to Book documents and UserInfo documents.
-    const userId = match.params._id;
-    const getUser = Meteor.users.findOne(userId);
-    const subscription = Meteor.subscribe('UserInfo');
-    const bookSub = Meteor.subscribe('UserBook');
-    return {
-        userInfo: UserInfo.findOne({username: getUser}),
-        books: Book.find({username: getUser}).fetch(),
-        ready: subscription.ready() && bookSub.ready(),
-    };
+export default withTracker(({ match }) => {
+  // Get access to Book documents and UserInfo documents.
+  const userId = match.params._id;
+  const getUser = Meteor.users.findOne(userId);
+  const subscription = Meteor.subscribe('UserInfo');
+  const bookSub = Meteor.subscribe('UserBook');
+  return {
+    userInfo: UserInfo.findOne({ username: getUser }),
+    books: Book.find({ username: getUser }).fetch(),
+    ready: subscription.ready() && bookSub.ready(),
+  };
 })(Profile);
